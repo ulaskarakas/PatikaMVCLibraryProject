ef3954e24706dec43750d9bd2558e2e4e8ff69cc
ulaskarakas <ulaskarakas95@gmail.com>
2025-02-20 14:28:55 +0300

    Remove unnecessary lines

--- PatikaMVCLibraryProject/Controllers/BookController.cs
+++ PatikaMVCLibraryProject/Controllers/BookController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using PatikaMVCLibraryProject.Models;
-using System.Reflection;
 
 namespace PatikaMVCLibraryProject.Controllers
 {
